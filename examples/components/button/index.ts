7bec7093047d9fe6f53681564527ea3db159dd20
xmweijh <xmweijh@foxmail.com>
2023-02-07 15:40:24 +0800

    fix: 🐛 组件组

--- examples/components/button/index.ts
+++ examples/components/button/index.ts
@@ -1,6 +1,7 @@
 import { withInstall } from '../../assets/utils/install';
 import Button from './src/button.vue';
+import ButtonGroup from './src/button-group.vue';
 
 export const JButton = withInstall(Button); // 增加类型
-
+export const JButtonGroup = withInstall(ButtonGroup);
 export default JButton;
