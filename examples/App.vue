74827be4a459d7b22df95adb1e62e5c4739efcc4
xmweijh <xmweijh@foxmail.com>
2023-02-14 22:14:27 +0800

    feat: ✨ tooltip组件

--- examples/App.vue
+++ examples/App.vue
@@ -11,6 +11,7 @@ import jFooter from './components/footer';
 import jDialog from './components/dialog';
 import { ref, reactive } from 'vue';
 import { jMessage } from './components/message';
+import { jTooltip } from './components/tooltip';
 import JCrumb from './components/crumb';
 // 消息框
 const open = (type: string) => {
@@ -147,6 +148,28 @@ const handleClose = (done: () => void) => {
     <div class="section">
       <j-crumb :brandsInfo="brandsInfo"></j-crumb>
     </div>
+    <div class="section">
+      <div class="tooltip-base-box">
+        <div class="row center">
+          <j-tooltip class="box-item" effect="dark" content="Top Center prompts info" placement="top">
+            <j-button>top</j-button>
+          </j-tooltip>
+        </div>
+        <div class="row">
+          <j-tooltip class="box-item" effect="dark" content="Left Center prompts info" placement="left">
+            <j-button class="mt-3 mb-3">left</j-button>
+          </j-tooltip>
+          <j-tooltip class="box-item" effect="dark" content="Right Center prompts info" placement="right">
+            <j-button>right</j-button>
+          </j-tooltip>
+        </div>
+        <div class="row center">
+          <j-tooltip class="box-item" effect="dark" content="Bottom Center prompts info" placement="bottom">
+            <j-button>bottom</j-button>
+          </j-tooltip>
+        </div>
+      </div>
+    </div>
     <div class="section"> ... </div>
   </div>
 </template>
@@ -204,6 +227,24 @@ const handleClose = (done: () => void) => {
       // color: var(--el-text-color-primary);
       text-align: center;
       // height: 150px;
+    }
+
+    .tooltip-base-box {
+      width: 600px;
+    }
+
+    .tooltip-base-box .row {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .tooltip-base-box .center {
+      justify-content: center;
+    }
+
+    .tooltip-base-box .box-item {
+      margin-top: 10px;
     }
   }
 }
