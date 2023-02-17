a9eae805a7fd887f1ba4d399e781f31c8e5ba7b4
zhangsan456 <102443344+zhangsan456@users.noreply.github.com>
2023-02-17 16:12:37 +0800

    第二添加input和时间选择

---
+++ examples/components/input/src/input.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import jButton from '../../button';
+const submit = (a) => {
+  console.log(a);
+  inputcontent = ''
+}
+const props = defineProps({
+  inputmessage: {
+    type: String,
+    default: ''
+  },
+
+})
+let inputcontent: any = ref()
+defineOptions({
+  name: ' JInput',
+});
+</script>
+
+<template>
+  <input type="text" placeholder="请输入内容" class="inputbox" v-model="inputcontent">
+  <j-button type="primary" @click="submit(inputcontent)">确认</j-button>
+</template>
+
+
+<style scoped lang="scss">
+.inputbox {
+  width: 240px;
+  height: 30px;
+  border: 1px solid black;
+  border-radius: 5px;
+  margin-right: 10px;
+
+  &::focus {
+    border-color: skyblue;
+    box-shadow: 0 0 3px skyblue;
+  }
+
+}
+</style>
