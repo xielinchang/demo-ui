48851ed8b2dc46e8689aef1031a036502a0f49e2
xmweijh <xmweijh@foxmail.com>
2023-02-19 13:42:56 +0800

    feat: ✨ 文档站样式

--- docs/docs/.vitepress/vitepress/component/Demo.vue
+++ docs/docs/.vitepress/vitepress/component/Demo.vue
@@ -8,27 +8,19 @@
         <component :is="formatPathDemos[path]" v-if="formatPathDemos[path]" />
       </div>
 
-      <div class="van-divider"></div>
-
       <div class="op-btns">
-        <span class="op-btn" @click="toggleSourceVisible()">查看源代码</span>
+        <span class="op-btn" @click="copyCode">复制</span>
+        <span class="op-btn" @click="toggleSourceVisible">显示源代码</span>
       </div>
 
-      <div v-show="sourceVisible"></div>
+      <VTransition>
+        <SourceCode v-show="sourceVisible" :source="source" />
+      </VTransition>
 
-      <Transition name="van-collapse-transition">
-        <div v-show="sourceVisible" class="example-source-wrapper">
-          <div class="example-source language-vue" v-html="decodedSource"></div>
-        </div>
-      </Transition>
-      
-      <Transition name="van-fade-in-linear">
-        <div
-          v-show="sourceVisible"
-          class="example-float-control"
-          @click="toggleSourceVisible()"
-        >
-          <span>隐藏代码</span>
+      <Transition name="fade">
+        <div v-show="sourceVisible" class="example-float-control" @click="toggleSourceVisible()">
+          <!-- <VIcon class="op-btn" name="icon-sanjiao2"></VIcon> -->
+          <span>隐藏源代码</span>
         </div>
       </Transition>
     </div>
@@ -37,6 +29,11 @@
 
 <script lang="ts" setup>
 import { ref, computed } from 'vue'
+import message from './message';
+import { useClipboard } from '@vueuse/core';
+import SourceCode from './v-source-code.vue';
+import VTransition from './v-transition.vue';
+
 const demos = import.meta.globEager('../../../examples/**/*.vue')
 
 const props = defineProps<{
@@ -59,75 +56,94 @@ const formatPathDemos = computed(() => {
   return demoObj
 })
 
-const sourceVisible = ref(false)
+const { copy, isSupported } = useClipboard({
+  source: decodeURIComponent(props.rawSource),
+});
 
-const decodedSource = computed(() => {
-  return decodeURIComponent(props.source)
-})
+const copyCode = async () => {
+  if (!isSupported) {
+    message.error('复制失败');
+  }
+  try {
+    await copy();
+    message.info('复制成功');
+  } catch (e: any) {
+    message.error('复制失败');
+  }
+};
+
+const sourceVisible = ref<boolean>(false);
+const toggleSourceVisible = () => {
+  sourceVisible.value = !sourceVisible.value;
+};
+
+
 const decodedDescription = computed(() =>
   decodeURIComponent(props.description!)
 )
 
-function toggleSourceVisible() {
-  sourceVisible.value = !sourceVisible.value
-}
+
 </script>
 
 <style lang="scss">
 .example-showcase {
   padding: 0.8rem;
   margin: 0.5px;
+
   >.van-icon {
     margin-right: 16px;
     font-size: 26px;
   }
 }
+
 .example {
-  border: 1px solid var(--vp-c-divider);
-  border-radius: 3px;
-  .op-btns {
-    padding: 0.5rem;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    height: 2.5rem;
-  }
-  .example-float-control {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-top: 1px solid var(--vp-c-divider);
-    height: 44px;
-    box-sizing: border-box;
-    border-bottom-left-radius: 4px;
-    border-bottom-right-radius: 4px;
-    margin-top: -1px;
-    color: var(--vp-c-brand-lighter);
-    cursor: pointer;
-    position: sticky;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 10;
+    border: 1px solid var(--vp-c-divider);
+    border-radius: 8px;
+    .op-btns {
+      padding: 0.5rem;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      height: 2.5rem;
+      font-size: 14px;
+      border-top: 1px solid var(--vp-c-divider);
 
-    &:hover {
-      color: var(--vp-c-brand);
+      .op-btn {
+        margin: 0 0.5rem;
+        cursor: pointer;
+        color: var(--vp-c-brand-lighter);
+        transition: 0.2s;
+        &:hover {
+          color: var(--vp-c-brand);
+        }
+      }
+    }
+    &-float-control {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-top: 1px solid var(--vp-c-divider);
+      height: 44px;
+      box-sizing: border-box;
+      // background-color: var(--bg-color, #fff);
+      border-bottom-left-radius: 4px;
+      border-bottom-right-radius: 4px;
+      margin-top: -1px;
+      color: var(--vp-c-brand-lighter);
+      cursor: pointer;
+      position: sticky;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      z-index: 10;
+      font-size: 14px;
+      span {
+        margin-left: 10px;
+      }
+
+      &:hover {
+        color: var(--vp-c-brand);
+      }
     }
   }
-  .example-source-wrapper {
-    .example-source.language-vue {
-      margin: 0 !important;
-      border-radius: 0;
-    }
-  }
-}
-
-.van-divider {
-  display: block;
-  height: 1px;
-  width: 100%;
-  margin: 0px 0;
-  border-top: 1px solid var(--vp-c-divider);
-}
-
 </style>
